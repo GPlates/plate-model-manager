f651ee572ff22f11890a23886db4415e7724df37
Michael Chin <michael.chin@sydney.edu.au>
2026-09-14 09:03:37 +1000

    try to get rid of  in version string

--- scripts/build-workflow.sh
+++ scripts/build-workflow.sh
@@ -6,9 +6,7 @@ pip3 install build twine
 python -m build
 twine check dist/*
 
-pip3 install pip-tools
 pip3 install -U sphinx sphinx_rtd_theme
-pip-compile pyproject.toml
 pip3 install .
 rm doc/source/plate_model_manager.rst
 rm doc/source/modules.rst
